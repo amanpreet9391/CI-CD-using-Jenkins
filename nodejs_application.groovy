a8aa0c1cb3da9c7854b67b1cb585d6fbcd866033
amanpreet9391 <amanpreet9391@gmail.com>
2020-07-05 17:43:08 +0530

    build and publish image to dockerhub

--- nodejs_application.groovy
+++ nodejs_application.groovy
@@ -14,6 +14,17 @@ wrappers{
 }
 steps{
     shell('npm install')
+    dockerBuildAndPublish{
+        repositoryName('amanpreet9391/simple-nodejs-app')
+       tag('${GIT_REVISION,length=9}')
+       registryCredentials('dockerhub')
+             forcePull(false)
+             forceTag(false)
+             createFingerprints(false)
+             skipDecorate()
+
+
+    }
 }
 
 
