e9ee7b4e43abe8d32802ea031e812fbaf4f39c35
amanpreet9391 <amanpreet9391@gmail.com>
2020-07-05 11:24:59 +0530

    docker installation step changed

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install docker-engine -y
 systemctl enable docker
-systemctl start docker
-sudo usermod -aG docker ec2-user
+sudo service docker start
+sudo groupadd docker && sudo usermod -aG docker ec2-user
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
