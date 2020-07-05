c5cb512b1b2a4331fa8a51d202be18154926ef10
amanpreet9391 <amanpreet9391@gmail.com>
2020-07-05 11:58:14 +0530

    final edits in setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install docker-engine -y
+sudo yum install docker -y
 systemctl enable docker
 sudo service docker start
-sudo groupadd docker && sudo usermod -aG docker ec2-user
-mkdir -p /var/jenkins_home
-chown -R 1000:1000 /var/jenkins_home/
+sudo groupadd docker 
+sudo usermod -aG docker ec2-user
+sudo mkdir -p /var/jenkins_home
+sudo chown -R 1000:1000 /var/jenkins_home/
