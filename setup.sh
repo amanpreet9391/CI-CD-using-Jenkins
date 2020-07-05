9d22fe1b4bf3df64d327d4022dc6d190599d9e18
amanpreet9391 <amanpreet9391@gmail.com>
2020-07-05 11:21:41 +0530

    edits in script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum update -y
-yum install docker-ce docker-ce-cli containerd.io
+sudo yum install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
-usermod -aG docker ec2-user
+sudo usermod -aG docker ec2-user
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
