dbebf81d7e84dcd040912e4e0985ae204aedd85b
amanpreet9391 <amanpreet9391@gmail.com>
2020-07-06 11:33:46 +0530

    Changing the dispalyed message

--- App/index.js
+++ App/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Guys! I hope you like my work :)');
-  res.send('This is a Green Deployment');
+  res.send('Hello Guys! I hope you like my work :) This is a Green Deployment');
+  
 });
 
 var server = app.listen(3000, function () {
